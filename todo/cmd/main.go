b6fc6b8f6007330dea7bd537921b525b708b6368
404th <umarov.doniyor.2002@gmail.com>
2022-11-15 15:01:46 +0500

    .env.example added

--- todo/cmd/main.go
+++ todo/cmd/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"log"
+
+	"github.com/404th/todo/server"
+)
+
+func main() {
+	srv := new(server.Server)
+
+	if err := srv.Run("8080"); err != nil {
+		log.Fatalf("error performed while starting server: %v", err)
+	}
+}
