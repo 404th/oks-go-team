fc39b26fdebd187f6960b2b4a76ecc8b31b14aa7
404th <umarov.doniyor.2002@gmail.com>
2022-11-16 12:47:10 +0500

    sdfsdf

--- todo/pkg/repository/auth_postgres.go
+++ todo/pkg/repository/auth_postgres.go
@@ -20,7 +20,7 @@ func NewAuthRepo(db *sqlx.DB) *AuthRepo {
 func (ar AuthRepo) CreateUser(user model.User) (int, error) {
 	var id int
 
-	query := fmt.Sprintf(`INSERT INTO %s (name, username, password_hash) RETURNING id`, usersTable)
+	query := fmt.Sprintf(`INSERT INTO %s (name, username, password_hash) VALUES ($1, $2, $3) RETURNING id;`, usersTable)
 	row := ar.db.QueryRow(query, user.Name, user.Username, user.Password)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
