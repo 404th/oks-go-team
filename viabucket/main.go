1952f3a395eeadb37e5f76c172172e3fdf4eed0e
404th <umarov.doniyor.2002@gmail.com>
2022-11-20 03:29:28 +0500

    dfsf

--- viabucket/main.go
+++ viabucket/main.go
@@ -2,59 +2,32 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 
-	"github.com/404th/viabucket/image"
-	"github.com/joho/godotenv"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// initializing .env files
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("error while loading .env: %s", err.Error())
-	}
 
-	// loading...
-	http.HandleFunc("/image", uploadImage)
+	e := gin.Default()
+	//set a lower memory limit for multipart forms (default is 32 Mib)
+	e.MaxMultipartMemory = 8 << 20 //8Mib
 
-	http.ListenAndServe(":9798", nil)
-}
-
-func uploadImage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("File Upload Endpoint Hit")
-
-	file, handler, err := r.FormFile("image")
-	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
-
-	// tempFile, err := ioutil.TempFile("imgs", "upload-*.jpg")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// defer tempFile.Close()
-
-	// read all of the contents of our uploaded file into a
-	// byte array
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	resp, err := image.ImageUpload(fileBytes)
-	if err != nil {
-		fmt.Println("error while uploading file: %w", err)
-		return
-	}
-
-	fmt.Println(resp)
-	fmt.Fprintf(w, resp.Location)
+	e.POST("/upload", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"mssage": err.Error(),
+			})
+		}
+		log.Println(file.Filename)                //print filename
+		dst := fmt.Sprintf("./%s", file.Filename) //The location of the build target file
+		//save the file to specific dst
+		c.SaveUploadedFile(file, dst)
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("%s uploaded!", file.Filename),
+		})
+	})
+	e.Run(":9090")
 }
