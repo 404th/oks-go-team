cd2e6613f7186fc5f9e094642e2a7ebc131d1072
404th <umarov.doniyor.2002@gmail.com>
2022-11-21 14:04:13 +0500

    sdsd

--- viabucket/main.go
+++ viabucket/main.go
@@ -5,10 +5,16 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/404th/viabucket/fileUploader"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error while loading .env: %s", err.Error())
+	}
 
 	e := gin.Default()
 	//set a lower memory limit for multipart forms (default is 32 Mib)
@@ -21,10 +27,12 @@ func main() {
 				"mssage": err.Error(),
 			})
 		}
-		log.Println(file.Filename)                //print filename
-		dst := fmt.Sprintf("./%s", file.Filename) //The location of the build target file
+		log.Println(file.Filename) //print filename
+		// dst := fmt.Sprintf("./%s", file.Filename) //The location of the build target file
 		//save the file to specific dst
-		c.SaveUploadedFile(file, dst)
+		// c.SaveUploadedFile(file, dst)
+
+		fileUploader.UploadFile(file)
 		c.JSON(http.StatusOK, gin.H{
 			"message": fmt.Sprintf("%s uploaded!", file.Filename),
 		})
